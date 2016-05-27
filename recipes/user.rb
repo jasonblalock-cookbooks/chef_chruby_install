42756629789c6e6419b15339997ec6887d8ad9b3
Jason Blalock <jasonblalock@users.noreply.github.com>
2016-05-27 19:09:45 -0400

    Reverting back to original

--- recipes/user.rb
+++ recipes/user.rb
@@ -19,11 +19,9 @@ node[:chruby_install][:users].each do |user, settings|
 
   template settings_file do
     source "chruby.sh.erb"
-    variables({
-      :settings => node[:chruby_install].merge(settings)
-    })
+    :settings => node[:chruby_install].merge(settings)
     owner user
-    group user_info['gid']
+    group user
     mode "0644"
   end
 
